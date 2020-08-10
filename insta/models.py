f85951148e730d2ff9c26f77fc33322b07d3382c
gkarumbi <gkarumbi@gmail.com>
2020-08-10 14:46:50 +0300

    Add Image class to create image objects

---
+++ insta/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='photos/', blank=True, null=True)
+    image_name = models.CharField(max_length=50)
+    image_caption = models.TextField()
+    profile = models.ForeignKey(Profile)
+    likes = models.IntegerField()
+    comments = models.TextField()
+
+    def __str__(self):
+        return self.image_name
+
+
+    def save_image(self):
+        self.save()
+
+    
+
+
+
+
+
+

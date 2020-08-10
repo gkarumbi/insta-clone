90430ce5d61d20f34eb59b02ab9a3001241b7044
gkarumbi <gkarumbi@gmail.com>
2020-08-10 16:35:41 +0300

    Add Profiles model

--- insta/models.py
+++ insta/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,8 +8,8 @@ class Image(models.Model):
     image_name = models.CharField(max_length=50)
     image_caption = models.TextField()
     profile = models.ForeignKey(Profile)
-    likes = models.IntegerField()
-    comments = models.TextField()
+    likes = models.IntegerField(default=0)
+    comments = models.TextField(default=0)
 
     def __str__(self):
         return self.image_name
@@ -19,14 +20,20 @@ class Image(models.Model):
 
     def delete_image(self):
         self.delete()
+    
+    def update_caption(self):
+        self.save()
 
-class Likes(models.Model):
+""" class Likes(models.Model):
     post_likes = models.ForeignKey(Image)
 
 class Comments(models.Model):
     post_comments = models.ForeignKey(Image)
+ """
 
 
 
-
-
+class Profile(models.Model):
+    user_name = models.ForeignKey(User)
+    profile =  models.ImageField(upload_to='profiles/', blank=True, null=True)
+    bio = models.TextField()

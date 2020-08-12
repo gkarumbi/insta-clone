1209faadfdef3c5f3b3767c4fbfd3d9f545ec5c8
gkarumbi <gkarumbi@gmail.com>
2020-08-13 01:01:25 +0300

    Add a created_date column to track time since the post was created

--- insta/models.py
+++ insta/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
@@ -7,9 +8,10 @@ class Image(models.Model):
     image = models.ImageField(upload_to='photos/', blank=True, null=True)
     image_name = models.CharField(max_length=50)
     image_caption = models.TextField()
-    profile = models.ForeignKey(Profile)
+    profile = models.ForeignKey(User,on_delete=models.DO_NOTHING)
     likes = models.IntegerField(default=0)
     comments = models.TextField(default=0)
+    created_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.image_name
@@ -34,6 +36,6 @@ class Comments(models.Model):
 
 
 class Profile(models.Model):
-    user_name = models.ForeignKey(User)
+    user_name = models.ForeignKey(User,on_delete=models.CASCADE)
     profile =  models.ImageField(upload_to='profiles/', blank=True, null=True)
     bio = models.TextField()
